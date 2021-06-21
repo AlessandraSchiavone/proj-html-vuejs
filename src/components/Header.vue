74c61f202b4314fd237a42d06b3ade00a70f5e7c
AlessandraSchiavone <alessandraschiavone31@gmail.com>
2021-06-21 14:33:37 +0200

    Aggiunta Nav-bar e Jumbotron statici

---
+++ src/components/Header.vue
@@ -0,0 +1,172 @@
+<template>
+  <header>
+      <section id="nav-bar">
+          <img src="../assets/img/logo-sidearea-1.png" alt="Logo Maree">
+          <ul class="list-inline">
+              <li ><span class="active">Home</span></li>
+              <li>Pages</li>
+              <li>Portfolio</li>
+              <li>Blog</li>
+              <li>Shop</li>
+              <li>Elements</li>
+              <li><img src="../assets/svg/svg-1.svg" alt="All"></li>
+          </ul>
+      </section>
+      <section id="jumbotron">
+          <div class="colsx col-lg-6">
+              <div class="col-lg-8 offset-lg-4">
+                <h1>Devotion that never <span class="coursive">ends</span></h1>
+                <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique, molestias!</h4>
+                <button id="bt-white"> Read More </button>
+              </div>
+
+              
+          </div>
+          <div class="coldx col-lg-6">
+              <div class="row-1">
+                  <img src="../assets/img/short-slider-rev-1-img-2.png" alt="">
+                  <img src="../assets/img/h-2-slider-img-14.png" alt="">
+              </div>
+              <div class="row-2">
+                    <div class="subrow-1">
+                        <img src="../assets/img/h-2-slider-img-12.png" alt=""> 
+                    </div>
+                    <div class="subrow-2">
+                      <img src="../assets/img/h-2-slider-img-13.png" alt="">  
+                    </div>
+                    <img src="../assets/img/h-2-slider-img-15.png" alt="">
+                    <img src="../assets/img/h-2-slider-img-16.png" alt="">
+                    <div class="subrow-3">
+                        <img src="../assets/img/short-slider-rev-1-img-6.png" alt="">
+                    </div>
+                    <div class="subrow-4">
+                     <img src="../assets/img/h-2-slider-img-17.png" alt=""> 
+                    </div>
+               </div>
+          </div>
+          
+      </section>
+  </header>
+</template>
+
+<script>
+export default {
+    name:"Header"
+}
+</script>
+
+<style lang="scss" scoped>
+#nav-bar{
+    display:flex;
+    justify-content: space-between;
+    align-items:center;
+    padding:40px 30px;
+    height: 80px;
+    img{
+        padding:5px;
+    }
+    .list-inline{
+        padding:0;
+        margin:0;
+        li{ 
+            display:inline;
+            padding:0px 20px;
+            text-transform: uppercase;
+            font-weight: 700;
+            font-size: 14px;
+            letter-spacing: 3px;
+            list-style:none;
+            line-height:19px;
+            cursor:pointer;
+            span{
+                display: inline-block;
+                &.active {
+                line-height: 6px;  
+                border-bottom:5px solid #F5D8CA;
+                }
+            }
+            img{
+                vertical-align: middle;
+            }
+        }
+    }
+}
+#jumbotron{
+    font-family: 'Roboto', sans-serif;
+    display: flex;
+    .colsx{
+      height:100%;
+      padding:50px 10px; 
+      h1{
+          padding-top:30px;
+          font-size:85px;
+          font-weight: 300;
+          .coursive{
+              font-family: 'Courgette', cursive;
+              font-weight: 600;
+          }
+      }
+      h4{
+          width:85%;
+          padding:5px 0px;
+          color:#A6A6A6;
+          font-weight: 100;
+          font-size: 22px;
+          line-height: 1.4;
+          letter-spacing: 1.2px;
+      }
+      button{
+          margin:30px 0px;
+          font-family: 'Open Sans', sans-serif;
+          text-transform: uppercase;
+          font-size:14px;
+          border:3px solid #E1C0B0;
+          letter-spacing: 2px;
+          font-weight:700;
+          background-color: white;
+          padding:18px 45px;
+          &:hover{
+              background-color:#E1C0B0;
+              color: white;
+          }
+      } 
+    }
+    .coldx{
+        height:100%;
+        padding:50px 0px;
+        .row-1{
+            width:660px;
+            display:flex;
+            justify-content: space-between;
+        }
+        .row-2{
+            display: flex;
+            position:relative;
+            .subrow-1{
+                position:absolute;
+                left:-80px;
+                bottom:180px;
+            }
+            .subrow-2{
+                position:absolute;
+                bottom:4px;
+                left:-10px;
+            }
+            .subrow-3{
+                position:absolute;
+                top:50px;
+                display:flex;
+                width:700px;
+                justify-content: flex-end;
+            }
+            .subrow-4{
+                position:absolute;
+                bottom:0px;
+                display:flex;
+                width:800px;
+                justify-content: flex-end; 
+            }
+        }    
+    }
+}
+</style>
